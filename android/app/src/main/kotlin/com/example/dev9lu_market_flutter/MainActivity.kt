4aad6bfcd66648a0be694d3c54cc4d329e3cdda0
jportillo8 <buttokk@gmail.com>
2022-05-15 15:05:54 -0500

    Primer commit

---
+++ android/app/src/main/kotlin/com/example/dev9lu_market_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dev9lu_market_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
